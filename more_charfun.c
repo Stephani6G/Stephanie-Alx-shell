4a63f071037ca32e896c42f8f863e0250f3cdc74
Stephani6G <stephaniegloria283@gmail.com>
2023-10-17 11:15:22 -0700

    more_charfun.c

--- more_charfun.c
+++ more_charfun.c
@@ -1,11 +1,12 @@
 #include "shell.h"
 
 /**
- * _strcmp - Compare Two String
- * @s1:String 1
- * @s2:String 2
- * Return: 0 If Identical Otherwise How Much Diffrent
+ * _strcmp - function thAT compares two strings
+ * @s1:String 1 to be compared
+ * @s2:String 2 to be compared
+ * Return: 0 If similar Otherwise the difference
  */
+
 int _strcmp(char *s1, char *s2)
 {
 int cmp = 0, i, len1, len2;
@@ -23,32 +24,38 @@ len2 = _strlen(s2);
 			cmp = s1[i] - s2[i];
 			break;
 		}
+
 		else
 			continue;
 	}
 	return (cmp);
 }
+
 /**
- * _isalpha - Check if Alphabtic
- *@c: Character
- * Return: 1 If True 0 If Not
+ * _isalpha -  function that checks if alphabet
+ *@c: Character to check if alphabet
+ * Return: 1 If true 0 if false success
  */
+
 int _isalpha(int c)
 {
 if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
 {
 return (1);
 }
+
 else
 {
 return (0);
 }
 }
+
 /**
- * _itoa - Convert Integer To Char
- * @n: Int To Convert
- * Return: Char Pointer
+ * _itoa -  a function that converts integer To char value
+ * @n: Int To Convert to char value
+ * Return: pointer to to char
  */
+
 char *_itoa(unsigned int n)
 {
 	int len = 0, i = 0;
@@ -70,12 +77,14 @@ char *_itoa(unsigned int n)
 	s[i + 1] = '\0';
 	return (s);
 }
+
 /**
- *  array_rev - Reverse Array
- * @arr:Array To Reverse
- * @len:Length Of Array
- * Return: Void(Reverse Array)
+ *  array_rev -  a function that reverses an array of pointers
+ * @arr:Array of pointers to Reverse
+ * @len:Length Of Array to reverse
+ * Return: Void success always
  */
+
 void array_rev(char *arr, int len)
 {
 	int i;
@@ -88,11 +97,13 @@ void array_rev(char *arr, int len)
 		arr[(len - 1) - i] = tmp;
 	}
 }
+
 /**
- * intlen - Determine Length Of Int
- * @num: Given Int
- * Return: Length Of Int
+ * intlen - a function that determinesthe length of an Int
+ * @num: int to determine
+ * Return: Length Of the integer
  */
+
 int intlen(int num)
 {
 	int len = 0;
