0a095f52516871735cdaa6e6a0ad1cec8bb06a52
TeddyDan <muretidanteddy1@gmail.com>
2023-10-17 00:28:01 -0700

    simple_shell

---
+++ subtract.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+        int a =2;
+        int b =1;
+        int diff = a+b;
+
+        printf("diff is %d\n",diff);
+
+        return 0;
+}
