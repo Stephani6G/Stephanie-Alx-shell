7f37dde1a72eb5d9adc21c5b4d83a1d5a4f9f1b0
Stephani6G <stephaniegloria283@gmail.com>
2023-10-19 03:26:51 -0700

    changes

--- oldshell.h
+++ oldshell.h
@@ -1,47 +1,6 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-#define DELIM " \t\r\n\a"
-#define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
-
-
-
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <linux/limits.h>
-
-/**###### environ var ######*/
-
-extern char **environ;
-
-/**##### MACROS ######*/
-
-#define BUFSIZE 1024
-#define DELIM " \t\r\n\a"
-#define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
-
-
-
-
-
-
-char *_strtok(char *str, const char *tok);
-unsigned int check_delim(char c, const char *str);
-char *_strncpy(char *dest, char *src, int n);
-int _strlen(char *s);
-int _putchar(char c);
-int _atoi(char *s);
-void _puts(char *str);
-int _strcmp(char *s1, char *s2);
-int _isalpha(int c);
 void array_rev(char *arr, int len);
 int intlen(int num);
 char *_itoa(unsigned int n);
