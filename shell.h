2d31c9de44957d2deb6be09672d829ced8736159
TeddyDan <muretidanteddy1@gmail.com>
2023-10-20 05:01:24 -0700

    simple_shell

--- shell.h
+++ shell.h
@@ -166,7 +166,6 @@ int is_delim(char, char *);
 int _isalpha(int);
 int _atoi(char *);
 
-
 /* toem_errors1.c */
 int _erratoi(char *);
 void print_error(info_t *, char *);
@@ -174,30 +173,24 @@ int print_d(int, int);
 char *convert_number(long int, int, int);
 void remove_comments(char *);
 
-
-
 /* toem_builtin.c */
 int _myexit(info_t *);
 int _mycd(info_t *);
 int _myhelp(info_t *);
 
-
 /* toem_builtin1.c */
 int _myhistory(info_t *);
 int _myalias(info_t *);
-
 
 /*toem_getline.c */
 ssize_t get_input(info_t *);
 int _getline(info_t *, char **, size_t *);
 void sigintHandler(int);
 
-
 /* toem_getinfo.c */
 void clear_info(info_t *);
 void set_info(info_t *, char **);
 void free_info(info_t *, int);
-
 
 /* toem_environ.c */
 char *_getenv(info_t *, const char *);
@@ -206,12 +199,10 @@ int _mysetenv(info_t *);
 int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
 
-
 /* toem_getenv.c */
 char **get_environ(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
-
 
 /* toem_history.c */
 char *get_history_file(info_t *info);
@@ -220,15 +211,12 @@ int read_history(info_t *info);
 int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
 
-
 /* toem_lists.c */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
-
-
 
 /* toem_lists1.c */
 size_t list_len(const list_t *);
